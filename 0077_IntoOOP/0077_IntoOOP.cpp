c6f630fc17bd2b0ad225352eea6a2b3b704e52da
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-04-22 14:35:37 +0700

    Add project files.

---
+++ 0077_IntoOOP/0077_IntoOOP.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public: // akses modifier
+    string nama;
+    int umur;
+    string jurusan;
+
+    void output() {
+        cout << "Nama: " << nama << endl;
+        cout << "Umur: " << umur << endl;
+        cout << "Jurusan: " << jurusan << endl;
+    }
+};
+ 
+    
