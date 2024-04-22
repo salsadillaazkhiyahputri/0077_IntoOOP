fe93e556beef33dde4cc2444d120e6035d1e09ca
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-04-22 14:38:31 +0700

    menandai bahwa anggota kelas dapat diakses

--- 0077_IntoOOP/0077_IntoOOP.cpp
+++ 0077_IntoOOP/0077_IntoOOP.cpp
@@ -2,16 +2,8 @@
 using namespace std;
 
 class Mahasiswa {
-public: // akses modifier
-    string nama;
-    int umur;
-    string jurusan;
-
-    void output() {
-        cout << "Nama: " << nama << endl;
-        cout << "Umur: " << umur << endl;
-        cout << "Jurusan: " << jurusan << endl;
-    }
+    public: // akses modifier
+        
 };
  
     
