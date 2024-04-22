e55b743e838c49f2aa218e8d58eccc29b4f5c7dd
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-04-22 15:00:08 +0700

    memasukkan sks ke dalam output

--- 0077_IntoOOP/0077_IntoOOP.cpp
+++ 0077_IntoOOP/0077_IntoOOP.cpp
@@ -34,6 +34,8 @@ class Matakuliah {
         void output() {
             cout << "Kode MK:  " << kodeMk << endl;
             cout << "Nama MK: " << namaMk << endl;
+            cout << "SKS: " << sks << endl;
+
 
 
         }
